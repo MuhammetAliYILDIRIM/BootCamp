d1c93b29def3cd2a2411405ec02eaa62d9b69582
MuhammetAliYILDIRIM <aliyil2019ali@gmail.com>
2020-01-15 20:57:25 +0000

    Student

--- Student.java
+++ Student.java
@@ -1,24 +1,43 @@
 import java.util.Scanner;
 
 public class Student {
+	Student() {
+
+	}
+
+	Student(String name, String surname, int number) {
+		this.name = name;
+		this.surname = surname;
+		this.number = number;
+	}
+
+	Student(String name, String surname, int number, String classOne, String classTwo, String classThree) {
+		this.name = name;
+		this.surname = surname;
+		this.number = number;
+		String[] classes = { classOne, classTwo, classThree };
+		autoenrol(classes);
+
+	}
+
 	String name;
 	String surname;
 	int number;
 	String[] enrolledClasses = new String[3];
 
 	public void autoenrol(String[] classes) {
-		for(int i = 0; i<classes.length;i++) {
-			enrolledClasses[i]=classes[i];
+		for (int i = 0; i < classes.length; i++) {
+			enrolledClasses[i] = classes[i];
 		}
 	}
-	
+
 	public void enrol() {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Please enter 3 classes");
-		
-		for(int i = 0 ; i < 3;i++) {
-			System.out.println("Enter "+i + " nd class");
-			enrolledClasses[i]=scan.nextLine();
+
+		for (int i = 0; i < 3; i++) {
+			System.out.println("Enter " + (i + 1) + " nd class");
+			enrolledClasses[i] = scan.nextLine();
 		}
 		scan.close();
 	}
@@ -38,25 +57,6 @@ public class Student {
 			}
 
 		}
-	}
-
-	Student() {
-
-	}
-
-	Student(String name, String surname, int number) {
-		this.name = name;
-		this.surname = surname;
-		this.number = number;
-	}
-	
-	Student(String name, String surname, int number,String classOne, String classTwo,String classThree) {
-		this.name = name;
-		this.surname = surname;
-		this.number = number;
-		String [] classes = {classOne,classTwo,classThree};
-		autoenrol(classes);
-		
 	}
 
 }
