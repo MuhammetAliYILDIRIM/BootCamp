242fa6a33f6668e4edcdac68acb260c5b2862041
MuhammetAliYILDIRIM <aliyil2019ali@gmail.com>
2020-02-13 21:00:31 +0000

    homework4

--- homework4/numberList.java
+++ homework4/numberList.java
@@ -6,23 +6,23 @@ import java.util.List;
 import java.util.Scanner;
 
 public class numberList {
-		public static void main(String[] args) {
-			Scanner scan = new Scanner(System.in);
-			List<Integer> numbers =new ArrayList<Integer>();
-			System.out.println("If you quit program enter -1");
-			while(true) {
-				try {
-					System.out.println("Enter a number ");
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		List<Integer> numbers = new ArrayList<Integer>();
+		System.out.println("If you quit program enter -1");
+		while (true) {
+			try {
+				System.out.println("Enter a number ");
 				numbers.add(Integer.parseInt(scan.nextLine()));
-				if((numbers.get(numbers.size()-1)==-1)) {
+				if ((numbers.get(numbers.size() - 1) == -1)) {
 					break;
 				}
 				System.out.println(numbers.toString());
-			
-				} catch (InputMismatchException e) {
-					System.out.println("You must enter a Integer");
-				}
-				
+
+			} catch (NumberFormatException e) {
+				System.err.println("You must enter a Integer");
 			}
+
 		}
+	}
 }
