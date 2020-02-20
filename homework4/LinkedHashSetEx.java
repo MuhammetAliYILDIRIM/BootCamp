ec45e6229c5cbc2aec36e67713ff17f053da2cd9
MuhammetAliYILDIRIM <aliyil2019ali@gmail.com>
2020-02-20 17:28:50 +0000

    homework 4

---
+++ homework4/LinkedHashSetEx.java
@@ -0,0 +1,63 @@
+package homework4;
+
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class LinkedHashSetEx {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		Set<Integer> linkedSet = new LinkedHashSet<>();
+
+		int howManyTimes = Integer.valueOf(scan.nextLine());
+		int queriesNumber = 0;
+		while (howManyTimes > 0) {
+			queriesNumber = Integer.valueOf(scan.nextLine());
+			while (queriesNumber > 0) {
+				switch (scan.next()) {
+				case "a":
+					linkedSet.add(scan.nextInt());
+					break;
+				case "b":
+					System.out.print(linkedSet.stream().sorted().collect(Collectors.toList()).toString()
+							.replace("[", "").replace("]", "").replace(",", "") + " ");
+					break;
+
+				case "c":
+					linkedSet.remove(scan.nextInt());
+					break;
+
+				case "d":
+					if (linkedSet.contains(scan.nextInt())) {
+						System.out.print(1 + " ");
+					} else {
+						System.out.print(-1 + " ");
+					}
+
+					break;
+				case "e":
+					System.out.print(linkedSet.size() + " ");
+					break;
+
+				case "f":
+					System.out.print(linkedSet.toString().replace("[", "").replace("]", "").replace(",", "") + " ");
+					break;
+
+				default:
+					System.out.println("Unvalid comment");
+					break;
+				}
+
+				queriesNumber--;
+			}
+			System.out.println();
+			scan.nextLine();
+			howManyTimes--;
+		}
+		scan.close();
+
+	}
+
+}
--- homework4/NumberListProgram.java
+++ homework4/NumberListProgram.java
@@ -1,35 +1,44 @@
 package homework4;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
-public class numberList {
+public class NumberListProgram {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		List<Integer> numbers = new ArrayList<Integer>();
-		System.out.println("If you enter -1,you quit the program");
-		int number;
-		while (true) {
+		System.out.println("Enter 10 numbers");
+		Integer number;
+		for (int i = 0; i < 10; i++) {
 			try {
-				System.out.println("Enter a number ");
+				System.out.print("\nEnter " + (i + 1) + ".nd number ");
 				number = Integer.parseInt(scan.nextLine());
-				if (number == -1) {
-					break;
-				}
+
 				if (numbers.contains(number)) {
 					System.out.println("the number has  aldready been in the list");
+					i = i - 1;
 					continue;
 				}
 				numbers.add(number);
 				System.out.println(numbers.toString());
 
 			} catch (NumberFormatException e) {
+				i = i - 1;
 				System.err.println("You must enter a Integer");
+
 			}
 
 		}
-		
+		Iterator<Integer> iterator = numbers.iterator();
+		while (iterator.hasNext()) {
+			if (iterator.next() % 2 == 1) {
+				iterator.remove();
+			}
+		}
+
+		System.out.println(numbers.toString());
 		scan.close();
 	}
 }
